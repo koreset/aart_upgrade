78e45f6f1228cd44aa8051b19fc80fa32b90e978
Jome Akpoduado <jome@koreset.com>
2024-07-21 08:38:26 +0200

    added reserves to valuation run results

---
+++ src/renderer/screens/valuations/gmm/RunDetail.vue
@@ -0,0 +1,339 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <base-card>
+          <template #header>
+            <span class="headline">Valuation Result Detail</span>
+          </template>
+          <template #default>
+            <v-row class="mb-3">
+              <v-col>
+                <v-btn variant="plain" :to="'/valuations/gmm/run-results'">
+                  {{ backButtonTitle }} </v-btn>
+              </v-col>
+            </v-row>
+            <base-card>
+              <template #header>
+                <span class="headline">Aggregated Results</span>
+              </template>
+              <template #default>
+                <v-row v-if="showSpCodeSelect">
+                  <v-col cols="3">
+                    <v-select v-model="selectedSpCode" variant="outlined" density="compact"
+                      placeholder="Select an SP Code" label="SP Code" :items="spCodes"
+                      @update:modelValue="displayAggDataForSpCode"></v-select>
+                  </v-col>
+                </v-row>
+                <data-grid :showExport="true" :rowData="rowData" :columnDefs="cDefs"></data-grid>
+              </template>
+            </base-card>
+            <base-card v-if="sapRowData.length > 0">
+              <template #header>
+                <span class="headline">Scoped Aggregated Results</span>
+              </template>
+              <template #default>
+                <v-row v-if="showSapSpCodeSelect">
+                  <v-col cols="3">
+                    <v-select v-model="selectedIfrs17Group" variant="outlined" density="compact"
+                      placeholder="Select a group" label="IFRS17 Group" :items="ifrs17groups"
+                      @update:modelValue="displaySapAggDataForIFRS17Group"></v-select>
+                  </v-col>
+                </v-row>
+
+                <data-grid :showExport="showExport" :rowData="sapRowData" :columnDefs="spCDefs"></data-grid>
+              </template>
+            </base-card>
+            <base-card>
+              <template #header>
+                <span class="headline">Reserves</span>
+              </template>
+              <template #default>
+                <v-row class="mt-5">
+                  <v-col cols="4">
+                    <v-select v-model="selectedSpCode" variant="outlined" density="compact"
+                      placeholder="Select a SP Code" label="SP Code" :items="spCodes"></v-select>
+                  </v-col>
+                  <v-col v-if="selectedSpCode !== null" cols="4">
+                    <v-select v-model="selectedVariable" variant="outlined" density="compact" :items="variableItems"
+                      label="Select a Variable"></v-select>
+                  </v-col>
+                  <v-col v-if="selectedSpCode !== null" cols="4">
+                    <v-btn variant="plain" rounded color="primary" @click="generateGraph">Go</v-btn>
+                  </v-col>
+                </v-row>
+
+                <!-- <v-skeleton-loader :loading="loading" height="300px" transition="scale-transition" type="image"> -->
+                <v-row v-if="chartSeries.data.length > 0">
+                  <v-col>
+                    <Chart class="chart" :options="chartOptions" :update-args="updateArgs"></Chart>
+                  </v-col>
+                </v-row>
+
+              </template>
+            </base-card>
+            <base-card></base-card>
+          </template>
+        </base-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import BaseCard from '@/renderer/components/BaseCard.vue'
+import DataGrid from '@/renderer/components/tables/DataGrid.vue'
+import { onMounted, ref, onBeforeMount } from 'vue';
+import { useRoute } from 'vue-router'
+import ProductService from '@/renderer/api/ProductService'
+import { Chart } from 'highcharts-vue'
+
+
+const route = useRoute()
+
+const showExport = ref(true)
+const backButtonTitle = '<  Back to Valuation Run List'
+const runId: any = ref(null)
+const loadingData = ref(false)
+const gridOptions: any = ref(null)
+const rowData: any = ref([])
+const sapRowData: any = ref([])
+const runSettings: any = ref(null)
+const spCodes: any = ref([])
+const runErrors: any = ref(null)
+const prodId: any = ref(null)
+const prodName: any = ref(null)
+const modelpointStats: any = ref(null)
+const cDefs = ref([])
+const spCDefs = ref([])
+const selectedVariable: any = ref("reserves")
+const selectedSpCode = ref(null)
+const aggData: any = ref([])
+const sapAggData: any = ref([])
+const showSpCodeSelect = ref(false)
+const showSapSpCodeSelect = ref(false)
+const selectedIfrs17Group = ref(null)
+const ifrs17groups: any = ref([])
+const variableItems = [
+  "reserves",
+  "death_outgo",
+  "premium_income",
+  "profit",
+  "initial_policy",
+  "commission",
+  "premium_not_received_lapse",
+  "clawback",
+  "accidental_death_outgo",
+  "cash_back_on_survival",
+  "cash_back_on_death",
+  "rider",
+  "expenses",
+]
+
+const updateArgs: any = ref([true, true, { duration: 1000 }])
+
+const chartSeries: any = ref({
+  data: [],
+  name: null,
+  color: null,
+})
+
+const chartOptions: any = ref({
+  credits: {
+    enabled: false,
+  },
+  chart: {
+    type: "spline",
+  },
+  title: {
+    text: "",
+  },
+  xAxis: {
+    categories: [],
+    title: { text: "Projection Month" },
+  },
+  yAxis: {
+    title: {
+      text: "Reserves",
+    },
+  },
+  series: [],
+})
+
+
+const displayAggDataForSpCode = () => {
+  rowData.value = [];
+  rowData.value = aggData.value.filter((item: any) => item.sp_code === spCodes.value[0]);
+  cDefs.value = createColumnDefs(rowData.value);
+}
+
+const displaySapAggDataForIFRS17Group = () => {
+  sapRowData.value = [];
+  sapRowData.value = sapAggData.value.filter((item: any) => item.IFRS17Group === selectedIfrs17Group.value);
+  spCDefs.value = createSapColumnDefs(sapRowData.value);
+}
+
+// const camelToSnakeCase = (str: String) => str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
+
+const generateGraph = () => {
+
+  // let variable = camelToSnakeCase(selectedVariable.value);
+  // if (variable[0] === "_") {
+  //   variable = variable.substring(1);
+  // }
+  getAggregatedVariableV2(selectedVariable.value, selectedSpCode.value);
+}
+
+
+const createColumnDefs = (rowData: any) => {
+  if (rowData === null || rowData.length === 0) {
+    return [];
+  }
+  if (rowData !== null && rowData.length > 0) {
+    const columnDefs: any = [];
+    const keys = Object.keys(rowData[0]);
+    keys.forEach((key) => {
+      columnDefs.push({
+        headerName: key,
+        field: key,
+        sortable: true,
+        filter: true,
+        resizable: true,
+        width: 150,
+      });
+    });
+    gridOptions.value.columnDefs = columnDefs;
+    return columnDefs;
+  }
+};
+
+const createSapColumnDefs = (rowData: any) => {
+  const columnDefs: any = [];
+  const keys = Object.keys(rowData[0]);
+  keys.forEach((key) => {
+    columnDefs.push({
+      headerName: key,
+      field: key,
+      sortable: true,
+      filter: true,
+      resizable: true,
+      width: 150,
+    });
+  });
+  gridOptions.value.sapColumnDefs = columnDefs;
+  return columnDefs;
+};
+
+const getModelPointStats = () => {
+  ProductService.getProductModelPointStats(prodId.value, runId.value).then(
+    (resp) => {
+      modelpointStats.value = resp.data;
+    }
+  );
+}
+
+const getAggregatedVariableV2 = (variable, spcode) => {
+  console.log("getAggregatedVariableV2", variable, spcode);
+  console.log("aggData", aggData.value);
+  chartSeries.value.data = [];
+  if (aggData.value !== null) {
+    aggData.value.forEach((elem: any) => {
+      if (
+        chartOptions.value.xAxis.categories.indexOf(elem.ProjectionMonth) ===
+        -1 &&
+        elem.sp_code === spcode
+      ) {
+        chartOptions.value.xAxis.categories.push(elem.ProjectionMonth);
+      }
+      if (elem.sp_code === spcode) {
+        chartSeries.value.data.push(elem[variable]);
+      }
+    });
+    chartSeries.value.name = prodName.value;
+    chartSeries.value.color = generateHexColorExcludingWhite();
+    chartOptions.value.series = [];
+    chartOptions.value.series.push(chartSeries.value);
+    chartOptions.value.yAxis.title.text = selectedVariable.value;
+  }
+}
+
+const generateHexColorExcludingWhite = () => {
+  const hexChars = '0123456789ABCDEF';
+  let color;
+  do {
+    color = '#';
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * hexChars.length);
+      color += hexChars[randomIndex];
+    }
+  } while (color === '#FFFFFF');
+  return color;
+}
+
+onBeforeMount(() => {
+  getAggregatedVariableV2("reserves", null);
+})
+
+onMounted(() => {
+  runId.value = route.params.id
+  prodId.value = route.params.prodId
+  prodName.value = route.params.prod_name
+
+  loadingData.value = true;
+  gridOptions.value = {};
+  ProductService.getValuationJob(runId.value).then((resp) => {
+    aggData.value = resp.data.projections;
+    spCodes.value = new Set(aggData.value.map((item) => item.sp_code));
+    spCodes.value = Array.from(spCodes.value);
+    console.log("sp codes", spCodes.value);
+
+
+    if (aggData.value.length > 5000) {
+      showSpCodeSelect.value = true;
+      rowData.value = aggData.value.filter((item: any) => item.sp_code === spCodes.value[0]);
+    } else {
+      showSpCodeSelect.value = false;
+      rowData.value = aggData.value
+    }
+
+    console.log("row data count", rowData.value.length);
+    if (rowData.value.length > 0) {
+      cDefs.value = createColumnDefs(rowData.value);
+    }
+
+    // scoped data
+    sapAggData.value = resp.data.scopedProjections;
+    console.log("row data count", sapAggData.value.length);
+    ifrs17groups.value = new Set(sapAggData.value.map((item) => item.IFRS17Group));
+    ifrs17groups.value = Array.from(ifrs17groups.value);
+
+
+    if (sapAggData.value.length > 5000) {
+      showSapSpCodeSelect.value = true;
+      sapRowData.value = sapAggData.value.filter((item: any) => item.IFRS17Group === ifrs17groups.value[0]);
+    } else {
+      showSapSpCodeSelect.value = false;
+      sapRowData.value = sapAggData.value;
+    }
+
+    if (sapRowData.value.length > 0) {
+      spCDefs.value = createSapColumnDefs(sapRowData.value);
+    }
+
+    runSettings.value = resp.data.settings;
+    spCodes.value = resp.data.spcodes;
+    console.log(resp.data);
+    if (resp.data.errors) {
+      runErrors.value = resp.data.errors;
+    }
+    loadingData.value = false;
+  });
+
+  getModelPointStats();
+  getAggregatedVariableV2("reserves", null);
+
+})
+
+</script>
+
+<style scoped></style>
---
+++ src/renderer/screens/valuations/gmm/RunResults.vue
@@ -0,0 +1,175 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <base-card>
+          <template #header>
+            <span class="headline">Valuation Run Results</span>
+          </template>
+          <template #default>
+            <v-expansion-panels>
+              <v-expansion-panel v-for="job in paginatedJobs" :key="job.id">
+                <v-expansion-panel-title>
+                  <template #default="{ expanded }">
+                    <v-row no-gutters>
+                      <v-col class="d-flex justify-start" cols="3">
+                        {{ job.run_name }}
+                      </v-col>
+                      <v-col class="text-grey" cols="9">
+                        <v-fade-transition leave-absolute>
+                          <span v-if="expanded" key="0">
+                            <v-list-item-subtitle v-if="job.status == 'In Progress'">
+                              Status: {{ job.status }} | Current Progress:
+                              {{ (job.points_done / job.total_points) * 100 }}%
+                            </v-list-item-subtitle>
+                            <v-list-item-subtitle v-else>
+                              Start: {{ formatDateString(job.creation_date) }} |
+                              Duration: {{ toMinutes(job.run_time) }} |
+                              Status: {{ job.status }} | User:
+                              {{ job.user_name }}
+                            </v-list-item-subtitle>
+
+                          </span>
+                          <span v-else key="1">
+                            <v-list-item-subtitle v-if="job.status == 'In Progress'">
+                              Status: {{ job.status }} | Current Progress:
+                              {{ (job.points_done / job.total_points) * 100 }}%
+                            </v-list-item-subtitle>
+                            <v-list-item-subtitle v-else>
+                              Start: {{ formatDateString(job.creation_date) }} |
+                              Duration: {{ toMinutes(job.run_time) }} |
+                              Status: {{ job.status }} | User:
+                              {{ job.user_name }}
+                            </v-list-item-subtitle>
+                          </span>
+                        </v-fade-transition>
+                      </v-col>
+                    </v-row>
+                  </template>
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                  <v-list-item v-for="run in job.products" :key="run.id">
+                    <v-list-item-title>
+                      <v-container>
+                        <v-row>
+                          <v-col cols="12">
+                            <p>{{ run.product_name }}</p>
+                            <v-btn v-if="job.status !== 'Failed'" variant="plain" rounded size="small" color="primary"
+                              :to="'/valuation-jobs/' +
+                run.id +
+                '/' +
+                run.product_id +
+                '/' +
+                run.product_name +
+                '/' +
+                job.run_name +
+                '/control'
+                ">View Control</v-btn>
+                            <v-btn v-if="job.status !== 'Failed'" variant="plain" rounded size="small" color="primary"
+                              :to="'/valuations/gmm/run-detail/' +
+                run.id +
+                '/' +
+                run.product_id +
+                '/' +
+                run.product_name +
+                '/' +
+                job.run_name
+                ">View Results</v-btn>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-list-item-title>
+                    <v-list-item-subtitle v-if="job.status == 'Failed'">
+                      <p>Run Error: {{ job.status_error }}</p>
+                    </v-list-item-subtitle>
+                  </v-list-item>
+                  <v-divider></v-divider>
+                  <v-btn variant="plain" rounded size="small" color="primary" @click="deleteRun(job.id)">Delete {{
+                job.run_name
+              }}</v-btn>
+                </v-expansion-panel-text>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </template>
+          <template #actions>
+            <v-pagination v-model="currentPage" :length="totalPages"></v-pagination>
+          </template>
+        </base-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import BaseCard from '../../../components/BaseCard.vue'
+import { computed, onMounted, ref } from 'vue'
+import ProductService from '../../../api/ProductService'
+import { DateTime } from 'luxon'
+
+const pageSize = 10;
+const currentPage = ref(1);
+const loading = ref(false)
+const runJobs = ref([])
+const totalPages = ref(3)
+let pollTimer: any = null;
+
+const paginatedJobs: any = computed(() => {
+  const start = (currentPage.value - 1) * pageSize;
+  const end = start + pageSize;
+  return runJobs.value.slice(start, end);
+});
+
+const formatDateString = (dateString: any) => {
+  return DateTime.fromISO(dateString).toLocaleString(DateTime.DATETIME_MED);
+}
+
+const toMinutes = (number: any) => {
+  number = number * 60;
+  const minutes = Math.floor(number / 60); // 7
+  let seconds = ((number % 60) / 100) * 60; // 30
+  seconds = Math.round(seconds);
+  return minutes + " m, " + seconds + " s";
+}
+
+const deleteRun = async (runId: any) => {
+  // loading.value = true;
+  // await ProductService.deleteValuationJob(runId);
+  // const res = await ProductService.getValuationJobs();
+  // runJobs.value = res.data;
+  // if (runJobs.value === undefined || runJobs.value === null) {
+  //   runJobs.value = [];
+  // }
+  // totalPages.value = Math.ceil(runJobs.value.length / pageSize);
+  // loading.value = false;
+}
+
+onMounted(async () => {
+  loading.value = true;
+  const res = await ProductService.getValuationJobs();
+  runJobs.value = res.data;
+  if (runJobs.value === undefined || runJobs.value === null) {
+    runJobs.value = [];
+  }
+  loading.value = false;
+  console.log(runJobs.value);
+  totalPages.value = Math.ceil(runJobs.value.length / pageSize);
+  console.log(totalPages.value);
+
+  if (
+    runJobs.value.length > 0 &&
+    runJobs.value.some((job: any) => job.status === "In Progress" || job.status === "Queued")
+  ) {
+    pollTimer = setInterval(() => {
+      if (runJobs.value.some((job: any) => job.status === "In Progress" || job.status === "Queued")) {
+        ProductService.getValuationJobs().then((response) => {
+          runJobs.value = response.data;
+        });
+      } else {
+        clearInterval(pollTimer);
+      }
+    }, 3000);
+  }
+})
+</script>
+
+<style scoped></style>
