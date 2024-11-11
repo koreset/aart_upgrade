75be8d0d4d8510a5a41fd1d0c8400909d46cf255
Jome Akpoduado <jome@koreset.com>
2024-11-11 02:28:43 +0200

    completed documentation module

--- src/renderer/screens/Documentation.vue
+++ src/renderer/screens/Documentation.vue
@@ -2,18 +2,12 @@
   <v-container>
     <v-row>
       <v-col>
-        <base-card elevation="3" class="rounded-lg">
+        <base-card :show-actions="false" elevation="3" class="rounded-lg">
           <template #header>
             <span class="headline">Documentation</span>
           </template>
           <template #default>
             <v-container>
-              <!-- <v-row>
-                <v-col cols="5">
-                  <pre><code :class="'language-' + language">{{ codeSnippet }}</code></pre>
-                </v-col>
-              </v-row> -->
-
               <v-row>
                 <v-col cols="4"
                   ><v-select
@@ -38,23 +32,19 @@
                   ></v-text-field>
                 </v-col>
               </v-row>
-              <v-row v-if="libraryfile.length > 0">
+              <v-row v-if="docfile.length > 0">
                 <v-col>
-                  <v-data-table
-                    :headers="headers"
-                    :items="libraryfile"
-                    :search="search"
-                  ></v-data-table>
+                  <v-data-table :headers="headers" :items="docfile" :search="search"></v-data-table>
                 </v-col>
               </v-row>
-              <!-- <v-row v-if="libraryfile.length > 0">
+              <v-row v-if="libraryFile.length > 0">
                 <v-col>
-                  <v-data-table :headers="headers" :items="libraryfile" :search="search">
-                    <template #item.data_variable="{ item }">
+                  <v-data-table :headers="headers" :items="libraryFile" :search="search">
+                    <template #[`item.data_variable`]="{ item }">
                       {{ item.data_variable }}
                       <v-tooltip right>
-                        <template #activator="{ on, attrs }">
-                          <v-btn icon v-bind="attrs" v-on="on">
+                        <template #activator="{ props }">
+                          <v-btn variant="plain" icon v-bind="props">
                             <v-icon>mdi-help-circle-outline</v-icon>
                           </v-btn>
                         </template>
@@ -65,7 +55,7 @@
                     </template>
                   </v-data-table>
                 </v-col>
-              </v-row> -->
+              </v-row>
             </v-container>
           </template>
         </base-card>
@@ -83,7 +73,7 @@ import { pricingLibrary } from '../data/pricing_library'
 import { experienceAnalysis } from '../data/experience_analysis'
 import BaseCard from '../components/BaseCard.vue'
 // import documentMarkdown from "../docs/base.md"
-import 'prismjs/themes/prism.css' // Import the desired theme
+import 'prismjs/themes/prism-dark.css' // Import the desired theme
 import Prism from 'prismjs/components/prism-core' // Import only the languages you need
 
 import { onMounted, ref } from 'vue'
@@ -91,7 +81,7 @@ import { onMounted, ref } from 'vue'
 // data
 const search = ref('')
 const docfile = ref([])
-const libraryfile = ref([])
+const libraryFile = ref([])
 const selectedTopic = ref(null)
 const topics = ref([
   'Modelpoints',
@@ -102,10 +92,7 @@ const topics = ref([
   'Experience Analysis'
 ])
 const headers: any = ref([])
-// const codeSnippet = ref(
-//   'IF (NOT (ISNULL ( [Modelpoint].[Modelpoint ID] ))) THEN\n\t[Modelpoint].[Modelpoint ID]\nELSE\n\tNULL\nENDIF'
-// )
-// const language = ref('javascript')
+const language: any = ref('javascript')
 
 // lifecycle
 onMounted(() => {
@@ -116,9 +103,9 @@ onMounted(() => {
 const getHelp = () => {
   console.log('selectedTopic.value: ', selectedTopic.value)
   docfile.value = []
-  libraryfile.value = []
+  libraryFile.value = []
   if (selectedTopic.value === 'PAA Library') {
-    libraryfile.value = paaLibrary
+    libraryFile.value = paaLibrary
     headers.value = [
       {
         title: 'Data Variable',
@@ -130,7 +117,7 @@ const getHelp = () => {
     ]
   }
   if (selectedTopic.value === 'GMM VFA Library') {
-    libraryfile.value = gmmVfaLibrary
+    libraryFile.value = gmmVfaLibrary
     headers.value = [
       {
         title: 'Data Variable',
@@ -142,7 +129,7 @@ const getHelp = () => {
     ]
   }
   if (selectedTopic.value === 'Pricing Library') {
-    libraryfile.value = pricingLibrary
+    libraryFile.value = pricingLibrary
     headers.value = [
       {
         title: 'Data Variable',
@@ -154,7 +141,7 @@ const getHelp = () => {
     ]
   }
   if (selectedTopic.value === 'Experience Analysis') {
-    libraryfile.value = experienceAnalysis
+    libraryFile.value = experienceAnalysis
     headers.value = [
       {
         title: 'Data Variable',
@@ -167,7 +154,7 @@ const getHelp = () => {
   }
 
   if (selectedTopic.value === 'Modelpoints') {
-    libraryfile.value = modelpointHelp
+    docfile.value = modelpointHelp
     headers.value = [
       {
         title: 'Data Variable',
@@ -183,7 +170,8 @@ const getHelp = () => {
   }
 
   if (selectedTopic.value === 'Features') {
-    libraryfile.value = featuresHelp
+    docfile.value = featuresHelp
+
     headers.value = [
       {
         title: 'Benefit Feature',
@@ -202,121 +190,5 @@ const getHelp = () => {
     ]
   }
 }
-
-// export default {
-//   data() {
-//     return {
-//       search: '',
-//       docfile: [],
-//       libraryfile: [],
-//       selectedTopic: null,
-//       topics: [
-//         'Modelpoints',
-//         'Features',
-//         'PAA Library',
-//         'GMM VFA Library',
-//         'Pricing Library',
-//         'Experience Analysis'
-//       ],
-//       headers: [],
-//       codeSnippet:
-//         'IF (NOT (ISNULL ( [Modelpoint].[Modelpoint ID] ))) THEN\n\t[Modelpoint].[Modelpoint ID]\nELSE\n\tNULL\nENDIF',
-//       language: 'javascript'
-//     }
-//   },
-//   mounted() {
-//     Prism.highlightAll()
-//   },
-//   methods: {
-//     getHelp() {
-//       docfile.value = []
-//       this.libraryfile = []
-//       if (this.selectedTopic == 'PAA Library') {
-//         this.libraryfile = paa_library
-//         this.headers = [
-//           {
-//             text: 'Data Variable',
-//             align: 'start',
-//             filterable: true,
-//             value: 'data_variable'
-//           },
-//           { text: 'Description', value: 'data_description', width: '400' }
-//         ]
-//       }
-//       if (this.selectedTopic == 'GMM VFA Library') {
-//         this.libraryfile = gmm_vfa_library
-//         this.headers = [
-//           {
-//             text: 'Data Variable',
-//             align: 'start',
-//             filterable: true,
-//             value: 'data_variable'
-//           },
-//           { text: 'Description', value: 'data_description', width: '400' }
-//         ]
-//       }
-//       if (this.selectedTopic == 'Pricing Library') {
-//         this.libraryfile = pricing_library
-//         this.headers = [
-//           {
-//             text: 'Data Variable',
-//             align: 'start',
-//             filterable: true,
-//             value: 'data_variable'
-//           },
-//           { text: 'Description', value: 'data_description', width: '400' }
-//         ]
-//       }
-//       if (this.selectedTopic == 'Experience Analysis') {
-//         this.libraryfile = experience_analysis
-//         this.headers = [
-//           {
-//             text: 'Data Variable',
-//             align: 'start',
-//             filterable: true,
-//             value: 'data_variable'
-//           },
-//           { text: 'Description', value: 'data_description', width: '400' }
-//         ]
-//       }
-
-//       if (this.selectedTopic == 'Modelpoints') {
-//         this.docfile = modelpointHelp
-//         this.headers = [
-//           {
-//             text: 'Data Variable',
-//             align: 'start',
-//             filterable: true,
-//             value: 'data_variable'
-//           },
-//           { text: 'Description', value: 'description', width: '600' },
-//           { text: 'Data Type', value: 'data_type' },
-//           { text: 'Constraints', value: 'constraints' },
-//           { text: 'Note', value: 'note', width: '600' }
-//         ]
-//       }
-
-//       if (this.selectedTopic === 'Features') {
-//         this.docfile = featuresHelp
-//         this.headers = [
-//           {
-//             text: 'Benefit Feature',
-//             align: 'start',
-//             filterable: true,
-//             value: 'benefit_feature'
-//           },
-//           { text: 'Description', value: 'description', width: '600' },
-//           { text: 'Product Association', value: 'product_association' },
-//           {
-//             text: 'Associated Required Tables',
-//             value: 'associated_required_tables',
-//             width: '400'
-//           },
-//           { text: 'Note', value: 'note', width: '600' }
-//         ]
-//       }
-//     }
-//   }
-// }
 </script>
 <style></style>
