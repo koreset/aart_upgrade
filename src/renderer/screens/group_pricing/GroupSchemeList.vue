3d81aaf20513783131557a0823ff195c4f0b98ed
Jome Akpoduado <jome@koreset.com>
2025-03-08 16:25:47 +0200

    updated scheme list view

--- src/renderer/screens/group_pricing/GroupSchemeList.vue
+++ src/renderer/screens/group_pricing/GroupSchemeList.vue
@@ -4,10 +4,10 @@
       <v-col>
         <base-card :show-actions="true">
           <template #header>
-            <span class="headline">Group Pricing Schemes</span>
+            <span class="headline">Group Pricing Schemes In Force</span>
           </template>
           <template #default>
-            <v-row class="mt-9 mx-5">
+            <!-- <v-row class="mt-9 mx-5">
               <v-col cols="3">
                 <v-text-field
                   v-model="schemeName"
@@ -28,28 +28,92 @@
                   >Add Group Scheme</v-btn
                 >
               </v-col>
-            </v-row>
-            <v-row v-if="rowData.length > 0" class="mx-5">
+            </v-row> -->
+            <!-- <v-row v-if="selectedScheme.length > 0">
+              <v-col class="d-flex justify-end">
+                <v-btn icon size="small" variant="plain" color="primary" @click="editScheme(null)">
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+                <v-btn icon variant="plain" size="small" color="error">
+                  <v-icon>mdi-delete</v-icon>
+                </v-btn>
+                <v-tooltip>
+                  <template #activator="{ props }">
+                    <v-btn icon color="primary" variant="plain" size="small" v-bind="props">
+                      <v-icon>mdi-eye</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>View Item</span>
+                </v-tooltip>
+                <v-tooltip>
+                  <template #activator="{ props }">
+                    <v-btn icon color="primary" variant="plain" size="small" v-bind="props">
+                      <v-icon>mdi-file-eye-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Submit for Review</span>
+                </v-tooltip>
+                <v-tooltip>
+                  <template #activator="{ props }">
+                    <v-btn icon color="primary" variant="plain" size="small" v-bind="props">
+                      <v-icon>mdi-file-send-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Generate Quote</span>
+                </v-tooltip>
+              </v-col>
+            </v-row> -->
+            <v-row v-if="schemes">
               <v-col>
-                <base-card :showActions="false">
-                  <template #header>
-                    <span class="headline">Scheme List</span>
+                <v-data-table
+                  v-model:model-value="selectedScheme"
+                  show-select
+                  select-strategy="single"
+                  class="table-row"
+                  density="compact"
+                  :headers="headers"
+                  :items="schemes"
+                  :search="search"
+                  return-object
+                >
+                  <!-- Slot for Actions Column -->
+                  <template #[`item.actions`]="{ item }: { item: any }">
+                    <v-btn
+                      v-if="selectedScheme[0].id === item.id"
+                      icon
+                      size="small"
+                      variant="plain"
+                      color="primary"
+                      @click="editScheme(item)"
+                    >
+                      <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
+                    <v-btn
+                      v-if="selectedScheme[0].id === item.id"
+                      icon
+                      variant="plain"
+                      size="small"
+                      color="error"
+                    >
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                    <v-tooltip>
+                      <template #activator="{ props }">
+                        <v-btn
+                          v-if="selectedScheme[0].id === item.id"
+                          icon
+                          color="primary"
+                          variant="plain"
+                          size="small"
+                          v-bind="props"
+                        >
+                          <v-icon>mdi-eye</v-icon>
+                        </v-btn>
+                      </template>
+                      <span>View Item</span>
+                    </v-tooltip>
                   </template>
-                  <template #default>
-                    <v-row>
-                      <v-col>
-                        <data-grid
-                          :columnDefs="columnDefs"
-                          :show-close-button="true"
-                          :rowData="rowData"
-                          :table-title="selectedTable"
-                          :pagination="true"
-                          @update:clear-data="clearData"
-                        />
-                      </v-col>
-                    </v-row>
-                  </template>
-                </base-card>
+                </v-data-table>
               </v-col>
             </v-row>
           </template>
@@ -63,50 +127,105 @@
 
 <script setup lang="ts">
 import BaseCard from '../../components/BaseCard.vue'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import ConfirmationDialog from '../../components/ConfirmDialog.vue'
 import GroupPricingService from '../../api/GroupPricingService'
-import DataGrid from '@/renderer/components/tables/DataGrid.vue'
 import formatValues from '@/renderer/utils/format_values'
 
 const confirmDialog = ref()
-const schemeName: any = ref('')
+// const schemeName: any = ref('')
 const schemes: any = ref([])
-const selectedTable = ref('Group Schemes In Force')
+const selectedScheme: any = ref([])
 
 const columnDefs: any = ref([])
 const rowData = ref([])
+const search = ref('')
 
-const createScheme = () => {
-  console.log('Creating Portfolio')
-  const schemPayload = {
-    name: schemeName.value
+const headers = computed(() => {
+  const baseHeaders: any = [
+    { title: 'Scheme Name', value: 'name' },
+    { title: 'In Force', value: 'in_force' },
+    { title: 'Annual Premium', value: 'annual_premium' },
+    { title: 'Duration in Force', value: 'duration_in_force' },
+    {
+      title: 'Renewal Date',
+      key: 'renewal_date',
+      width: '20%',
+      value: (item: any) => parseDateString(item.renewal_date)
+    },
+    { title: 'Member Count', value: 'member_count' },
+    { title: 'Earned Premium', value: 'earned_premium' }
+  ]
+
+  if (selectedScheme.value.length > 0) {
+    baseHeaders.unshift({ title: 'Actions', value: 'actions', align: 'center', sortable: false })
   }
-  GroupPricingService.createScheme(schemPayload).then((res) => {
-    schemes.value.push(res.data)
-    schemeName.value = null
-  })
+
+  return baseHeaders
+})
+// [
+// { title: 'Scheme Name', value: 'name', key: 'name' },
+// { title: 'In Force', value: 'in_force', key: 'in_force' },
+// { title: 'Annual Premium', value: 'annual_premium', key: 'annual_premium' },
+// { title: 'Duration in Force', value: 'duration_in_force' },
+// {
+//   title: 'Renewal Date',
+//   key: 'renewal_date',
+//   width: '20%',
+//   value: (item: any) => parseDateString(item.renewal_date)
+// },
+// { title: 'Member Count', value: 'member_count' },
+// { title: 'Earned Premium', value: 'earned_premium' }
+// { title: 'PTD', value: 'ptd_benefit' },
+// { title: 'CI', value: 'ci_benefit' },
+// { title: 'Funeral', value: 'family_funeral_benefit' },
+// { title: 'Basis', value: 'basis' },
+// { title: 'Status', value: 'status' },
+
+// { title: 'Submitted By', value: 'created_by' },
+// { title: 'Reviewer', value: 'reviewer' },
+// { title: 'Actions', value: 'actions', align: 'center' as 'center', sortable: false }
+// ]
+
+console.log('headers:', headers)
+
+const parseDateString = (dateString) => {
+  const date = new Date(dateString)
+  const formattedDate = date.toISOString().split('T')[0]
+  return formattedDate
 }
+
+// const createScheme = () => {
+//   console.log('Creating Portfolio')
+//   const schemPayload = {
+//     name: schemeName.value
+//   }
+//   GroupPricingService.createScheme(schemPayload).then((res) => {
+//     schemes.value.push(res.data)
+//     schemeName.value = null
+//   })
+// }
 
 // const deleteBroker = async (id: number) => {
 //   console.log('Deleting Portfolio', id)
 // }
 
+const editScheme = (item) => {
+  console.log('editing:', item)
+}
+
 onMounted(() => {
   GroupPricingService.getSchemesInforce().then((res) => {
     if (res.data.length > 0) {
       rowData.value = res.data
-      console.log(res.data)
+      schemes.value = res.data
+      console.log('schemes:', res.data)
       createColumnDefs(res.data)
     } else {
       schemes.value = []
     }
   })
 })
-
-const clearData = () => {
-  rowData.value = []
-}
 
 const createColumnDefs = (data: any) => {
   columnDefs.value = []
@@ -124,4 +243,26 @@ const createColumnDefs = (data: any) => {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.table-row {
+  white-space: nowrap;
+}
+
+::v-deep(.v-data-table thead th) {
+  background-color: #223f54 !important;
+  color: white;
+  text-align: center;
+  font-weight: bold;
+  white-space: nowrap;
+  min-width: 150px;
+}
+
+.search-box {
+  width: 100%;
+}
+.v-table__wrapper > table > thead {
+  background-color: #223f54 !important;
+  color: white;
+  white-space: nowrap;
+}
+</style>
